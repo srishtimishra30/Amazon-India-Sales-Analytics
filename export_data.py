b1fb6a2733b6417c6f3afd40f3608532293a5cb2
Srishti Mishra <srishtimishra302004@gmail.com>
2026-06-05 12:06:13 +0530

    Update export_data.py

--- export_data.py
+++ export_data.py
@@ -7,6 +7,6 @@ tables = ['orders','inventory','pricing','international_sales','order_prediction
 for t in tables:
     df = pd.read_sql(f"SELECT * FROM {t}", engine)
     df.to_csv(rf'C:\Users\srish\Desktop\Amazon-fashion\{t}.csv', index=False)
-    print(f"✅ {t}.csv saved — {len(df)} rows")
+    print(f"{t}.csv saved — {len(df)} rows")
 
-print("\n🎉 All CSVs ready for Power BI!")
+print("\n All CSVs ready for Power BI!")
